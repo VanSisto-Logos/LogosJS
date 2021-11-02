78fa5efc626045185acfa8968b2e0ef20a96e189
Andrii Omelchuk <andrii.omelchuk@openexc.com>
2021-11-02 19:58:44 +0200

    lesson1: done

---
+++ scripts/script.js
@@ -0,0 +1,75 @@
+num = 2.2;
+str = "String"
+bool = true
+n = null
+u = undefined
+
+// console.log(num)
+
+// var v = 2;
+let l = 3;
+const PI = 3.14
+
+// alert("fdsa");
+// resultConfirm = confirm("Are you shure?")
+// console.log(resultConfirm)
+
+// userName = prompt("Enter name")
+// console.log(userName)
+
+// a = ["String", 22, false]
+
+// for(key in a) {
+//     console.log(a[key])
+// }
+
+// nnn = "Stirng" 
+// console.log(isNaN(nnn))
+
+st = "2"
+// nn = parseInt(st)
+// console.log("st \"2\" isNan " + isNaN(nn))
+
+// nn = st - 0
+
+// nn2 = 6
+// st2 = 6 + ""
+
+// console.log(nn)
+
+// i = 1;
+// i += 2;
+
+// console.log(0 == false)
+// console.log("" == 0)
+// console.log("" == false)
+
+// console.log(0 === false)
+// console.log("" === 0)
+// console.log("" === false)
+
+// name1 = "Visa"
+// name2 = "Vatyaffdsafsdafsda"
+
+// console.log(name1 > name2)
+
+// b = 5 > 3 ? "tr" : "fl";
+// console.log(b)
+
+// switch (b) {
+//     case "true":
+//     case "tr": console.log("TRUE"); break;
+//     case "fl": console.log("FALSE"); break;
+//     default: console.log("DEFAULT");
+// }
+
+a = [6,4,7,9,45,2,4,6]
+console.log(Math.max(...a))
+console.log(Math.min(...a))
+console.log(Math.round(2.5))
+console.log(Math.round((Math.random() * 100)))
+console.log(Math.floor(2.9))
+console.log(Math.ceil(2.1))
+
+
+console.log(Math.abs(5))
